dd08181319f6e19db2c2003eaabc166cecdecf2c
MrKanji <164999554+MrKanji@users.noreply.github.com>
2024-07-31 11:55:33 +0700

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.get('/', (req, res) => {
 });
 
 
-const statusMessages = ["Đang chơi ---------- City","🏢 UNIFIED POLICE DEPARTMENT 🏢","🏙️ LOS SANTOS POLICE DEPARTMENT 🏙️","🏜️ BLAINE COUNTY SHERIFF'S OFFICE 🏜️","✅ TO PROTECT AND TO SERVE ✅","👮 Ban Quản Lý Cảnh Sát UPD 👮","🛠️ Nhận setup Discord & PD theo yêu cầu 🛠️","📞 Contact Discord: thuyalwayssmiles93 📞"];
+const statusMessages = ["Đang chơi JC ROLEPLAY City","🏢UNIFIED POLICE DEPARTMENT🏢","🏙️LOS SANTOS POLICE DEPARTMENT🏙️","🏜️BLAINE COUNTY SHERIFF'S OFFICE🏜️","🚧CENTRAL INVESTIGATION BUREAU🚧","✅TO PROTECT AND TO SERVE✅","👮Ban Quản Lý Cảnh Sát UPD👮","🛠️Nhận setup Discord & PD theo yêu cầu🛠️","📞Contact Discord: thuyalwayssmiles93📞"];
 
 
 let currentIndex = 0;
